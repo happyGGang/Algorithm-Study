bc9ac5b7cdc8269e788890ceded59048ea6fa717
happyGGang <96277834+happyGGang@users.noreply.github.com>
2023-04-24 11:39:32 +0900

    [unrated] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,8 @@
+function solution(str1, str2) {
+    let arr = []
+    for(let i = 0; i < str1.length; i++) {
+        arr.push(str1[i])
+        arr.push(str2[i])
+    }
+    return arr.join('');
+}
